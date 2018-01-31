3bd857d7226e1488ea556b604909afb5ea751025
silent-sea1119 <sandragonzales56t@gmail.com>
2018-01-31 15:08:14 +0100

    Project template

---
+++ template/server/__init__.py
@@ -0,0 +1,9 @@
+"""
+  @author Victor I. Afolabi
+  
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  
+  Created on 31 January, 2018 @ 3:07 PM.
+  Copyright © 2018. Victor. All rights reserved.
+"""
