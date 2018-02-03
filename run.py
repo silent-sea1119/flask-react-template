fed5a41c1cb6ef346d12c5fb16dece65bf46a4f8
silent-sea1119 <sandragonzales56t@gmail.com>
2018-02-03 15:27:14 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 """
   @author Victor I. Afolabi
-  
+
   A.I. Engineer & Software developer
   javafolabi@gmail.com
-  
+
   Created on 31 January, 2018 @ 4:09 PM.
   Copyright © 2018. Victor. All rights reserved.
 """
@@ -24,7 +24,7 @@ def run_webpack():
 
 
 if __name__ == '__main__':
-    from flask_react_template.server.views import app
+    from flask_react_template.server import app
     # process = multiprocessing.Process(target=run_webpack)
     # process.start()
     # process.join()
